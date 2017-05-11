c96ecc3909157ab940210deed89dfe0acc897098
dnunezm <comodore13@hotmail.com>
2017-05-10 20:52:14 -0400

    Clase agregada Producto

--- repuestos/repuestos2/src/main/java/com/diegonunez/ipoo/repuestos2/datos/cliente/Producto.java
+++ repuestos/repuestos2/src/main/java/com/diegonunez/ipoo/repuestos2/datos/cliente/Producto.java
@@ -6,5 +6,36 @@ public class Producto {
 	private String description;
 	private Byte imagen;
 	private Double precio;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Byte getImagen() {
+		return imagen;
+	}
+	public void setImagen(Byte imagen) {
+		this.imagen = imagen;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
 
 }
