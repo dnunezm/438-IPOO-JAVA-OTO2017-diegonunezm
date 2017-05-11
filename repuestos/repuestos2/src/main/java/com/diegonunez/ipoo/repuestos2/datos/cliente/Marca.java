442bd73a53c03cb53ffbd6304395e3e81dd104c7
dnunezm <comodore13@hotmail.com>
2017-05-10 21:14:49 -0400

    Clase Marca agregada

--- repuestos/repuestos2/src/main/java/com/diegonunez/ipoo/repuestos2/datos/cliente/Marca.java
+++ repuestos/repuestos2/src/main/java/com/diegonunez/ipoo/repuestos2/datos/cliente/Marca.java
@@ -1,5 +1,45 @@
 package com.diegonunez.ipoo.repuestos2.datos.cliente;
 
-public class Marca {
+import java.util.Date;
 
+public class Marca {
+	private int id;
+	private String nombre;
+	private String description;
+	
+	//contructor 1
+	public Marca(){
+	}
+     //constructor 2
+	public Marca(int id, String nombre, String description) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.description = description;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 }
