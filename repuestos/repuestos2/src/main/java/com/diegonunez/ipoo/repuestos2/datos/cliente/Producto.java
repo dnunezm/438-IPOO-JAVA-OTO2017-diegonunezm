1b6673d5da0b1fd5e7aad1de6e049b00babeae4b
dnunezm <comodore13@hotmail.com>
2017-05-10 20:48:49 -0400

    Clase nueva agregada Producto

---
+++ repuestos/repuestos2/src/main/java/com/diegonunez/ipoo/repuestos2/datos/cliente/Producto.java
@@ -0,0 +1,10 @@
+package com.diegonunez.ipoo.repuestos2.datos.cliente;
+
+public class Producto {
+	private int id;
+	private String nombre;
+	private String description;
+	private Byte imagen;
+	private Double precio;
+
+}
